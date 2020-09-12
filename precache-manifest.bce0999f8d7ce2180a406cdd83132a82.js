652a7a8556d4bd85dbb0d2c88d2e260ec85c8285
Piotr Kałużny <kaluzny.piotr1997@gmail.com>
2020-09-12 19:11:27 +0200

    Updates

---
+++ precache-manifest.bce0999f8d7ce2180a406cdd83132a82.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933ed08bf05cd57179d779c3f7420ac8",
+    "url": "/jolmar/index.html"
+  },
+  {
+    "revision": "ba17d9438cc8d984f6c2",
+    "url": "/jolmar/static/css/2.c997a826.chunk.css"
+  },
+  {
+    "revision": "f064846d96648e530881",
+    "url": "/jolmar/static/css/main.23b2b68a.chunk.css"
+  },
+  {
+    "revision": "ba17d9438cc8d984f6c2",
+    "url": "/jolmar/static/js/2.f978a621.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jolmar/static/js/2.f978a621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f064846d96648e530881",
+    "url": "/jolmar/static/js/main.737a4d3a.chunk.js"
+  },
+  {
+    "revision": "9a276cba62fa3e46f889",
+    "url": "/jolmar/static/js/runtime-main.0445497b.js"
+  },
+  {
+    "revision": "62e80a6add3491a13dfa40b5de9e4873",
+    "url": "/jolmar/static/media/0.62e80a6a.jpg"
+  },
+  {
+    "revision": "b3cf57db7279aaea1e4a73e1c6b83fc4",
+    "url": "/jolmar/static/media/1.b3cf57db.jpg"
+  },
+  {
+    "revision": "ad5e86a3730b3eb69c754aa32a7eb613",
+    "url": "/jolmar/static/media/2.ad5e86a3.jpeg"
+  },
+  {
+    "revision": "eaf7d4909ed527aaf8dc5a9a19217ced",
+    "url": "/jolmar/static/media/3.eaf7d490.jpg"
+  },
+  {
+    "revision": "fdd9364d06d4787b9623f45ef2936f0c",
+    "url": "/jolmar/static/media/4.fdd9364d.jpg"
+  },
+  {
+    "revision": "ccb8f6a0c72e5ac1319c24e6a2d290ad",
+    "url": "/jolmar/static/media/attic.ccb8f6a0.jpg"
+  },
+  {
+    "revision": "f986395fac1530892dafa5e3509b476f",
+    "url": "/jolmar/static/media/call.f986395f.png"
+  },
+  {
+    "revision": "8e5a921e8f1a82d708a3c4f428221faf",
+    "url": "/jolmar/static/media/clipAr.8e5a921e.png"
+  },
+  {
+    "revision": "69d27c178a07fda05ce0ad8bb44c54ff",
+    "url": "/jolmar/static/media/door.69d27c17.jpg"
+  },
+  {
+    "revision": "4593a3d68837806862564b3cd0b4d342",
+    "url": "/jolmar/static/media/garderobe.4593a3d6.jpg"
+  },
+  {
+    "revision": "2038c684684727ae15b931898b39fef4",
+    "url": "/jolmar/static/media/hall.2038c684.jpg"
+  },
+  {
+    "revision": "142fc1fd1c834c52d31119b85ab72b9e",
+    "url": "/jolmar/static/media/kitchen.142fc1fd.jpg"
+  },
+  {
+    "revision": "5a80353d7f6c5a6033c29705ee6f02d0",
+    "url": "/jolmar/static/media/kitchen.5a80353d.jpeg"
+  },
+  {
+    "revision": "7e991f5cf93c15b6edadb6a368c13abc",
+    "url": "/jolmar/static/media/office.7e991f5c.jpg"
+  },
+  {
+    "revision": "e4597f602eb1dddcd72c69f1da292732",
+    "url": "/jolmar/static/media/orderFurnitures.e4597f60.jpg"
+  },
+  {
+    "revision": "3f3cb678ed8d7ca4ef51059eaf389fa7",
+    "url": "/jolmar/static/media/swipeUp.3f3cb678.png"
+  },
+  {
+    "revision": "c220ed45e788fa6ea50db18e53ecbe03",
+    "url": "/jolmar/static/media/wardrobe.c220ed45.jpg"
+  }
+]);
